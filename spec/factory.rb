ee7b966a85a3eef4807a612c5f538bc9eb866f5e
rachelbookhout <rbookhou@gmail.com>
2014-10-02 15:17:07 -0400

    Set up views and user model and testing

---
+++ spec/factory.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+
+  factory :user do
+    first_name "Brad"
+    last_name "Pitt"
+    sequence :email do |n|
+      "hottie#{n}@hotmail.com"
+    end
+    password: hottie88
+  end
+
+end
