2343874b2686673adc5ab28b8ee2ff8abd39c1aa
rachelbookhout <rbookhou@gmail.com>
2014-10-02 17:32:49 -0400

    Fixed it so the root page loads up

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -3,5 +3,19 @@ class RequestsController < ApplicationController
   def index
   end
 
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def delete
+  end
 
 end
