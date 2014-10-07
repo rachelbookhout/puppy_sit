7c04d4184a901f9922a7e63f09103e0487bbe9b2
rachelbookhout <rbookhou@gmail.com>
2014-10-07 11:09:47 -0400

    Working Requester

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,11 @@
 class Review < ActiveRecord::Base
   belongs_to :reviewer, class_name: "User"
-  belongs_to :reviewable, polymorphic: true
+  belongs_to :reviewable, class_name: "User"
+
+  belongs_to :request
   validates :title, presence:true
   validates :body, presence:true
   validates :rating, presence:true, :inclusion => 1..5
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,18 @@
 class User < ActiveRecord::Base
   mount_uploader :profile_photo, ProfilePhotoUploader
-  has_many :requests
+  has_many :requests, foreign_key: :requester_id
   has_many :comments
   has_many :responses, foreign_key: :responder_id
-  has_many :requests, through: :responses
-  has_many :reviews, :as => :reviewable
+
+  has_many :reviews, as: :reviewable, source: :requester
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  def responder_reviews
+  end
+
+  def requester_reviews
+  end
 end
