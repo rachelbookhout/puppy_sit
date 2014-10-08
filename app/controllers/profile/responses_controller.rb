9a4d8618f2146a40ae7332d5ad576e427cd2497b
rachelbookhout <rbookhou@gmail.com>
2014-10-08 15:46:15 -0400

    User Response Index and Destroy work

--- app/controllers/profile/responses_controller.rb
+++ app/controllers/profile/responses_controller.rb
@@ -2,5 +2,15 @@ class Profile::ResponsesController < ApplicationController
     before_filter :current!
 
   def index
+    @user = current_user
+    @responses = @user.responses
   end
+
+  def destroy
+  @user = current_user
+  @response = Response.find(params[:id])
+  @response.destroy
+  redirect_to profile_user_responses_path(@user.id)
+  end
+
 end
