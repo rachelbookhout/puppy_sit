f8755d76440729a298c662fa1b985951d9341f5f
rachelbookhout <rbookhou@gmail.com>
2014-10-08 16:43:25 -0400

    Added possible reviews

--- app/controllers/profile/reviews_controller.rb
+++ app/controllers/profile/reviews_controller.rb
@@ -2,5 +2,9 @@ class Profile::ReviewsController < ApplicationController
     before_filter :current!
 
   def index
+  @user = User.find(params[:user_id])
+  # @reviews_given = Review.where("reviewer_id" == @user.id)
+  @requesters = User.user_reviews_requester(@user)
+  @responders = User.user_reviews_responder(@user)
   end
 end
