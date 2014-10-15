1a90ad8bc0500e50f4d04589e37f818669906904
rachelbookhout <rbookhou@gmail.com>
2014-10-15 16:52:07 -0400

    Check

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,12 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 Time::DATE_FORMATS[:times] = "%B %d at %I:%M %p"
+ActionMailer::Base.smtp_settings = {
+    :port =>           '587',
+    :address =>        'smtp.mandrillapp.com',
+    :user_name =>      ENV['MANDRILL_USERNAME'],
+    :password =>       ENV['MANDRILL_APIKEY'],
+    :domain =>         'heroku.com',
+    :authentication => :plain
+}
+ActionMailer::Base.delivery_method = :smtp
