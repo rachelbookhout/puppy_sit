82c03b358d9181f27cab670cd26757216d772509
rachelbookhout <rbookhou@gmail.com>
2014-10-16 19:30:58 -0400

    Welcome page done

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,8 @@ Rails.application.routes.draw do
    resources :responses, only:[:create]
   end
   resources :responses, only:[:index]
-  root to:'requests#index'
+  resources :welcome, only:[:index]
+  root to:'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
