b85b6995167a1ffee49ffb13210ca1fd422d9a70
rachelbookhout <rbookhou@gmail.com>
2014-10-16 11:34:02 -0400

    Dependent Destory

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   mount_uploader :profile_photo, ProfilePhotoUploader
-  has_many :requests, foreign_key: :requester_id
+  has_many :requests, foreign_key: :requester_id, dependent: :destroy
   has_many :comments
-  has_many :responses, foreign_key: :responder_id
+  has_many :responses, foreign_key: :responder_id, dependent: :destroy
   has_many :reviews, as: :reviewable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
